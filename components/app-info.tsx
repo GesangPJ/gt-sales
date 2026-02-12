3711213eba2a55702813454561e82d10091f8bd3
G.PAUDRA <g.paudra@gmail.com>
2026-02-12 21:14:11 +0700

    ganti version

--- components/app-info.tsx
+++ components/app-info.tsx
@@ -1,11 +1,12 @@
 "use client"
 
 export function AppInfo() {
-//   const name = process.env.NEXT_PUBLIC_APP_NAME
+// const name = process.env.NEXT_PUBLIC_APP_NAME
+const versi = process.env.NEXT_PUBLIC_APP_VERSION
 
   return (
     <div className="text-sm text-muted-foreground">
-      <span>v.2026.2.7</span>
+      <span>v.{versi}</span>
     </div>
   )
 }
