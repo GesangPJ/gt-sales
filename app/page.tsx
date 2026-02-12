ff88549ee3fafdf88e066b606c95565f1aedfa65
G.PAUDRA <g.paudra@gmail.com>
2026-02-12 21:13:55 +0700

    update halaman utama
    cek session

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,18 @@
 
 import LoginForm from "@/components/login-form";
+import { redirect } from "next/navigation"
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
 
-export default function Page() {
+export default async function Page() {
+    const session = await auth.api.getSession({
+    headers: await headers(),
+  })
+
+    // jika sudah ada session, langsung ke kasir
+    if (session) {
+    redirect("/kasir")
+    }
 return (
     <div>
         <LoginForm/>
