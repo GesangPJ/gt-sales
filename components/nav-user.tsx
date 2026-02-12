8324bf4dda7d9b4e1ca9b580b79efea0e6acbaea
G.PAUDRA <g.paudra@gmail.com>
2026-02-12 19:24:51 +0700

    Update nav-user.tsx

--- components/nav-user.tsx
+++ components/nav-user.tsx
@@ -32,6 +32,8 @@ import {
   useSidebar,
 } from "@/components/ui/sidebar"
 
+import { Menu } from '@base-ui/react/menu'
+
 interface NavUserProps {
   user: {
     name: string
@@ -58,69 +60,82 @@ export function NavUser({ user }: NavUserProps) {
 
   return (
     <SidebarMenu>
-      <SidebarMenuItem>
-        <DropdownMenu>
-          <DropdownMenuTrigger render={<SidebarMenuButton
-              size="lg"
-              className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
-            />}>
-            
-              <Avatar className="h-8 w-8 rounded-lg grayscale">
-                <AvatarImage src={user.avatar} alt={user.name} />
-                <AvatarFallback className="rounded-lg">CN</AvatarFallback>
-              </Avatar>
-              <div className="grid flex-1 text-left text-sm leading-tight">
-                <span className="truncate font-medium">{user.name}</span>
-                <span className="text-muted-foreground truncate text-xs">
-                  {user.email}
-                </span>
-              </div>
-              <IconDotsVertical className="ml-auto size-4" />
-          </DropdownMenuTrigger>
-          <DropdownMenuContent
-            className="w-(--radix-dropdown-menu-trigger-width) min-w-56 rounded-lg"
-            side={isMobile ? "bottom" : "right"}
-            align="end"
-            sideOffset={4}
+  <SidebarMenuItem>
+    <Menu.Root>
+      <Menu.Trigger className="w-full" render={<SidebarMenuButton
+          size="lg"
+          className="data-open:bg-sidebar-accent 
+                     data-open:text-sidebar-accent-foreground"
+        >
+          <Avatar className="h-8 w-8 rounded-lg grayscale">
+            <AvatarImage src={user.avatar} alt={user.name} />
+            <AvatarFallback className="rounded-lg">CN</AvatarFallback>
+          </Avatar>
+
+          <div className="grid flex-1 text-left text-sm leading-tight">
+            <span className="truncate font-medium">{user.name}</span>
+            <span className="text-muted-foreground truncate text-xs">
+              {user.email}
+            </span>
+          </div>
+
+          <IconDotsVertical className="ml-auto size-4" />
+        </SidebarMenuButton>}>
+        
+      </Menu.Trigger>
+
+      <Menu.Portal>
+        <Menu.Positioner
+          side={isMobile ? "bottom" : "right"}
+          align="end"
+          sideOffset={4}
+        >
+          <Menu.Popup
+            className="min-w-56 rounded-lg border bg-popover p-1 shadow-md"
           >
-            <DropdownMenuLabel className="p-0 font-normal">
-              <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
-                <Avatar className="h-8 w-8 rounded-lg">
-                  <AvatarImage src='/avatars/shadcn.jpg' alt={user.name} />
-                  <AvatarFallback className="rounded-lg">AD</AvatarFallback>
-                </Avatar>
-                <div className="grid flex-1 text-left text-sm leading-tight">
-                  <span className="truncate font-medium">{user.name}</span>
-                  <span className="text-muted-foreground truncate text-xs">
-                    {user.email}
-                  </span>
-                </div>
+            <div className="px-2 py-2">
+              <div className="text-sm font-medium">{user.name}</div>
+              <div className="text-xs text-muted-foreground">
+                {user.email}
               </div>
-            </DropdownMenuLabel>
-            <DropdownMenuSeparator />
-            <DropdownMenuGroup>
-              <Link href="/pengaturan">
-              <DropdownMenuItem>
-                  <IconUserCircle /> 
-                  <p className="font-bold">Akun</p>
-              </DropdownMenuItem>
+            </div>
+
+            <Menu.Separator />
+
+            <Menu.Item>
+              <Link
+                href="/pengaturan"
+                className="flex items-center gap-2"
+              >
+                <IconUserCircle />
+                <span>Akun</span>
               </Link>
-              <Link href="/panduan">
-              <DropdownMenuItem>
-              <IconHelp/>
-              Panduan
-            </DropdownMenuItem>
+            </Menu.Item>
+
+            <Menu.Item>
+              <Link
+                href="/panduan"
+                className="flex items-center gap-2"
+              >
+                <IconHelp />
+                <span>Panduan</span>
               </Link>
-              
-            </DropdownMenuGroup>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem onClick={handleLogout}>
+            </Menu.Item>
+
+            <Menu.Separator />
+
+            <Menu.Item
+              onClick={handleLogout}
+              className="flex items-center gap-2"
+            >
               <IconLogout />
-              Keluar
-            </DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
-      </SidebarMenuItem>
-    </SidebarMenu>
+              <span>Keluar</span>
+            </Menu.Item>
+          </Menu.Popup>
+        </Menu.Positioner>
+      </Menu.Portal>
+    </Menu.Root>
+  </SidebarMenuItem>
+</SidebarMenu>
   )
 }
