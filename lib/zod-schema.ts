1bdace3989dd9509517deffb6a20755c44022f3b
G.PAUDRA <g.paudra@gmail.com>
2026-02-12 21:12:42 +0700

    zod schema

--- lib/zod-schema.ts
+++ lib/zod-schema.ts
@@ -7,6 +7,14 @@ export const loginSchema = z.object({
   password: z.string().min(8, "Password minimal 8 karakter"),
 })
 
+export const kategoriSchema = z.object({
+  nama: z.string().trim().min(1, "Nama kategori harus diisi!")
+})
+
+export const brandSchema = z.object({
+  nama: z.string().trim().min(1,"Nama brand harus diisi!")
+})
+
 export const registrasiSchema = z.object({
     email: z.email("Format email tidak valid").trim().min(1, "Email wajib diisi").toLowerCase(),
     name: z.string().trim().min(1, "Nama wajib diisi"),
@@ -16,6 +24,8 @@ export const registrasiSchema = z.object({
     alamat: z.string().trim().max(250).optional().or(z.literal("")),
 })
 
+export type BrandSchema = z.infer<typeof brandSchema>
+export type KategoriSchema = z.infer<typeof kategoriSchema>
 export type LoginSchema = z.infer<typeof loginSchema>
 export type RegistrasiSchema = z.infer<typeof registrasiSchema>
 
