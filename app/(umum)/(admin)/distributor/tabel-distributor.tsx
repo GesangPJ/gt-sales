96b778392e75f952fdf658c65e7b24e154827b7a
G.PAUDRA <g.paudra@gmail.com>
2026-02-13 18:54:20 +0700

    tabel distributor berfungsi

---
+++ app/(umum)/(admin)/distributor/tabel-distributor.tsx
@@ -0,0 +1,45 @@
+
+// Komponen Tabel Distributor
+
+import { DataTable } from "@/components/data-table"
+import { baseUrl } from "@/lib/base-url"
+import { switchAPI } from "@/lib/select-API"
+import { kolom_distributor, Distributor } from "./kolom-distributor"
+import { toast } from "sonner"
+
+export default async function TabelDistributor(){
+
+    let distributors: Distributor[] = []
+
+    try{
+
+        const respon = await fetch(`${baseUrl}${switchAPI}/data-distributor`,{
+            next:{
+                tags:['distributor'],
+                revalidate:86400,
+            }
+        })
+
+        if(!respon.ok){
+            toast.error("Gagal ambil data distributor")
+        }
+
+        const hasil = await respon.json()
+        distributors = hasil.data
+
+
+    }catch(error:any){
+        console.error("Error ambil data distributor", error)
+    }
+
+    return(
+        <div>
+            <DataTable
+            columns={kolom_distributor}
+            data={distributors}
+            />
+
+        </div>
+    )
+}
+
