6c24a46e34732b5898e5f03899990287d1eacba8
G.PAUDRA <g.paudra@gmail.com>
2026-02-14 20:18:46 +0700

    update header

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -1,4 +1,3 @@
-import { Button } from "@/components/ui/button"
 import { Separator } from "@/components/ui/separator"
 import { SidebarTrigger } from "@/components/ui/sidebar"
 import { AmbilTanggal } from "./ambiltanggal"
@@ -13,7 +12,6 @@ export function SiteHeader() {
           orientation="vertical"
           className="mx-2 data-[orientation=vertical]:h-8"
         />
-        {/* <h1 className="text-base font-medium">Documents</h1> */}
         <h1 className="text-lg font-medium"><AmbilTanggal/></h1>
         <div className="ml-auto flex items-center gap-2">
           <SwitchDarkMode />
