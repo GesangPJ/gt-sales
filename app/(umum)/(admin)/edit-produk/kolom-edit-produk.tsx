cbad9cf9968d35df89001eb08e02b2418df5a625
G.PAUDRA <g.paudra@gmail.com>
2026-02-15 14:19:45 +0700

    kolom edit produk

---
+++ app/(umum)/(admin)/edit-produk/kolom-edit-produk.tsx
@@ -0,0 +1,147 @@
+// Kolom produk untuk edit
+
+"use client"
+
+import { ColumnDef } from "@tanstack/react-table"
+import { Button } from "@/components/ui/button"
+import { ArrowUpDown} from "lucide-react"
+import Link from "next/link"
+
+export type EditProduk = {
+    id: string
+    nama_produk: string
+    harga_beli: number
+    harg_jual: number
+    namadistributor: string
+}
+
+export const KolomEditProduk: ColumnDef<EditProduk>[] = [
+    {
+    id: "no",
+    // header: "No",
+    header: () => {
+      return (
+          <div className="text-center w-1.5">No.</div>
+      )
+    },
+    size: 10,
+    cell: ({ row, table }) => {
+      // Index berdasarkan row yang sedang ditampilkan (respect sorting & filtering)
+      const sortedRows = table.getSortedRowModel().flatRows
+      const index =
+        sortedRows.findIndex((r) => r.id === row.id)
+      return <p className="text-center">{index + 1}.</p>
+    },
+    enableSorting: false,
+  },
+  {
+    accessorKey: "nama_produk",
+    size:50,
+    header: ({ column }) => (
+      < Button variant="ghost" onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        className="h-8 px-2"
+      >
+        Nama Produk
+        <ArrowUpDown className="ml-2 h-4 w-4" />
+      </Button>
+    ),
+    cell: ({ row }) => (
+      <div className="capitalize pl-2">
+        {row.getValue("nama_produk")}
+      </div>
+    ),
+  },
+  {
+    accessorKey: "harga_jual",
+    header: ({ column }) => {
+      return (
+        <div className="w-8">
+          <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+          className="text-left"
+        >
+          Harga Jual
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+        </div>
+      )
+    },
+    cell: ({ row }) => {
+      const harga = parseFloat(row.getValue("harga_jual"))
+      
+      // Format ke Rp
+      const formatted = new Intl.NumberFormat("id-ID", {
+        style: "currency",
+        currency: "IDR",
+        minimumFractionDigits: 0,  // Hilangkan .00
+      }).format(harga)
+
+      return <div className="text-left font-medium pl-2">{formatted}</div>
+    },
+  },
+  {
+    accessorKey: "harga_beli",
+    header: ({ column }) => {
+      return (
+        <div className="w-8">
+          <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Harga Beli
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+        </div>
+        
+      )
+    },
+    cell: ({ row }) => {
+      const harga = parseFloat(row.getValue("harga_beli"))
+      
+      // Format ke Rp
+      const formatted = new Intl.NumberFormat("id-ID", {
+        style: "currency",
+        currency: "IDR",
+        minimumFractionDigits: 0,  // Hilangkan .00
+      }).format(harga)
+
+      return <div className="text-left font-medium pl-2">{formatted}</div>
+    },
+  },
+  {
+    accessorKey: "namadistributor",
+    size:50,
+    header: ({ column }) => (
+      < Button variant="ghost" onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        className="h-8 px-2"
+      >
+        Distributor
+        <ArrowUpDown className="ml-2 h-4 w-4" />
+      </Button>
+    ),
+    cell: ({ row }) => (
+      <div className="capitalize pl-2">
+        {row.getValue("namadistributor")}
+      </div>
+    ),
+  },
+  {
+    id: "detail",
+    header:" ",
+    size:32,
+    minSize:20,
+    cell: ({row}) => {
+      const id = row.original.id
+
+      return (
+        <div className="flex justify-center">
+        <Button size="sm" variant="outline">
+          <Link href={`/edit-produk/${id}`}>Edit</Link>
+        </Button>
+      </div>
+      ) 
+    }
+  },
+]
+
