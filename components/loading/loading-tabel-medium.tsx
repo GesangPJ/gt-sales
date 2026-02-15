1e126da8ad659254a78602328872e9c831f65875
G.PAUDRA <g.paudra@gmail.com>
2026-02-15 09:43:33 +0700

    komponen loading

---
+++ components/loading/loading-tabel-medium.tsx
@@ -0,0 +1,16 @@
+
+// Komponen Skeleton Tabel Medium (1/2 halaman LG)
+
+import { Skeleton } from "../ui/skeleton"
+
+export default function LoadingTabelMedium(){
+
+    return(
+        <div>
+            
+        </div>
+    )
+
+}
+
+
