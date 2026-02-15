e091e66492b6f24dbf5d49c2355c71d2c535dae5
G.PAUDRA <g.paudra@gmail.com>
2026-02-15 18:05:38 +0700

    API data akun

---
+++ app/api/v1/(akun)/data-akun/route.ts
@@ -0,0 +1,43 @@
+
+// API Data Akun
+
+import { NextResponse } from "next/server";
+import prisma from "@/lib/prisma";
+
+export async function GET(){
+
+    try{
+
+        const data_akun = await prisma.user.findMany({
+            select:{
+                id:true,
+                name:true,
+                email:true,
+                alamat:true,
+                notelp:true,
+                createdAt:true,
+                updatedAt:true,
+            }
+        })
+
+        if(!data_akun){
+            console.log("Data Akun kosong!")
+            return NextResponse.json({
+                message:"Data akun kosong!"
+            }, {status: 404})
+        }
+
+        return NextResponse.json({
+            message:"Data akun berhasil diambil",
+            data: data_akun,
+        }, {status:200})
+
+    }catch(error){
+
+        console.error("Error API Akun", error)
+
+        return NextResponse.json({
+            message:"Server Error API Akun"
+        }, {status:500})
+    }
+}
