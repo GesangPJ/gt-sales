e0946c52d4473db25816777cdad968d14e1e6dbc
G.PAUDRA <g.paudra@gmail.com>
2026-02-21 18:04:11 +0700

    halaman edit data akun

---
+++ app/(umum)/(admin)/akun/edit-akun/page.tsx
@@ -0,0 +1,17 @@
+
+// Halaman edit data akun
+
+import FormEditAkun from "./form-edit-akun"
+
+
+export default function HalamanEditAkun(){
+
+
+    return(
+        <div>
+            <FormEditAkun/> 
+        </div>
+    )
+}
+
+
