e3bc365c1d4dc0dbd0f30e05b47ee664e8e48f3c
G.PAUDRA <g.paudra@gmail.com>
2026-02-22 06:12:44 +0700

    halaman pembelian produk

---
+++ app/(umum)/(admin)/pembelian/page.tsx
@@ -0,0 +1,15 @@
+
+// Halaman Pembelian Produk
+
+import FormPembelian from "./form-pembelian"
+
+export default function HalamanPembelian(){
+
+return(
+    <div>
+      <h1 className="text-xl text-center font-bold">Pembelian Produk</h1>
+      <FormPembelian/> 
+    </div>
+)
+}
+
