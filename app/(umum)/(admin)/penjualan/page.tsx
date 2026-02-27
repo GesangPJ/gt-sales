66e60d71b60291a60184916952c7f4674badfee5
G.PAUDRA <g.paudra@gmail.com>
2026-02-27 07:03:32 +0700

    update halaman kasir

---
+++ app/(umum)/(admin)/penjualan/page.tsx
@@ -0,0 +1,14 @@
+
+// Halaman daftar penjualan produk
+import TabelPenjualan from "./tabel-penjualan"
+
+export default function HalamanDaftarPenjualan(){
+    return(
+        <div>
+            <h1 className="text-2xl text-center mb-5">Daftar Penjualan</h1>
+            <TabelPenjualan/>
+        </div>
+    )
+}
+
+
