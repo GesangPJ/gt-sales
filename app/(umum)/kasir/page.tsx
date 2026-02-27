4137bb21ce76d697719207edb99020605dae85eb
G.PAUDRA <g.paudra@gmail.com>
2026-02-27 07:04:36 +0700

    update halaman kasir

--- app/(umum)/kasir/page.tsx
+++ app/(umum)/kasir/page.tsx
@@ -7,7 +7,6 @@ export default function Kasir(){
 
     return(
         <div>
-            <h1 className="text-2xl font-mono mb-7">Halaman Kasir</h1>
             <FormPenjualan/>
         </div>
     )
